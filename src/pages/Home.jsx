70bf7442380a11b80bc05934ae35983ffa6b9f87
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2023-05-14 10:01:46 +0300

    1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
+import axios from "../axios";
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
+export const Home = () => {
+  const dispatch = useDispatch();
+  // posts потому что в store.js указан posts, а сам posts ведет на postreducer в котором посты и теги
+  const { posts, tags } = useSelector((state) => state.posts);
+  const isPostLoaded = posts.status === "loading";
+  const isTagsLoaded = tags.status === "loading";
+  //
+  console.log(tags.items[0]);
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, []);
+  return (
+    <>
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Новые" />
+        <Tab label="Популярные" />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostLoaded ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostLoaded ? (
+              // это нужно чтобы не выдавало ошибку, тк при первой итерации элемента массива будут undefined
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl}
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={3}
+                tags={obj.tags}
+                isEditable
+              />
+            )
+          )}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items[0]} isLoading={isTagsLoaded} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: "Вася Пупкин",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                },
+                text: "Это тестовый комментарий",
+              },
+              {
+                user: {
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                },
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
